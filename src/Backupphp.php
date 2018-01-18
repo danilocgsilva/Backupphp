c1688a6c5a0bb0b804ddfd725b94415d7075d978
Danilo Silva <danilocgsilva@gmail.com>
2018-01-17 23:25:20 -0200

    Refactor to better checking about to split as value

--- src/Backupphp.php
+++ src/Backupphp.php
@@ -185,7 +185,7 @@ class Backupphp
     private function _tableInsertsData($table) {
 
         $columns = $this->_fetchTableColumns($table);
-        $string_columns = implode(", ", $columns);
+        $string_columns = implode(", ", $columns[0]);
         
         $query_loop_data = "SELECT * FROM `{$table}`";
         $stmt = $this->_pdo->prepare($query_loop_data);
@@ -203,32 +203,80 @@ class Backupphp
         }
     }
 
+    /**
+     * Return an array of two arrays: The first one containing the the array of field
+     *  names. The second one, containing the fields types
+     *
+     * @param string $table The name's table
+     * 
+     * @return array
+     */
     private function _fetchTableColumns($table)
     {
         $query_select_column = "DESCRIBE {$table}";
-        $results = $this->_pdo->query($query_select_column, PDO::FETCH_NUM);
+        $results = $this->_pdo->query($query_select_column, PDO::FETCH_ASSOC);
         
         $columns_array = [];
+        $columns_types_array = [];
 
         foreach($results as $column) {
-            $columns_array[] = $column[0];
+            $columns_array[]      = $column['Field'];
+            $columns_type_array[] = $column['Type'];
         }
 
-        return $columns_array;
+        return [ $columns_array, $columns_type_array ];
     }
 
-    private function _generateValuesFromQueryRowResult($row_result, $fields_array)
+    /**
+     * Generates the string values to be inserted in the insert statement
+     *
+     * @param array $row_result   The result row provided by a query execution
+     * @param array $fields_array The array of fields to loop through
+     * 
+     * @return string
+     */
+    private function _generateValuesFromQueryRowResult($row_result, $columns_and_types)
     {
         $results_array = [];
 
-        foreach ($fields_array as $field) {
-            $results_array[] = $this->_pdo->quote(
-                $row_result[$field]
-            );
+        // foreach ($columns_and_types[0] as $field) {
+        //     $results_array[] = $this->_decidesTermType();
+        // }
+
+        for ($i = 0; $i < count($columns_and_types); $i++) {
+            $results_array[] = $this->_decidesTermType($columns_and_types, $i);
         }
 
         $string_values = implode(",", $results_array);
 
         return $string_values;
     }
+
+    /**
+     * Decides what it will append to the string that will be inserted in the insert 
+     *  statement
+     *
+     * @param array $columns_and_types The array containing the array of column name 
+     *                                 and the one with the column type
+     * @param int   $loopValue         In which iteration does it will test
+     * 
+     * @return string
+     */
+    private function _decidesTermType($columns_and_types, $loopValue)
+    {
+        $field_name = $columns_and_types[0][$loopValue];
+        $field_type = $columns_and_types[1][$loopValue];
+
+        $valueStringfied = $this->_pdo->quote($field_name);
+
+        $finalValue = "";
+        if ($valueStringfied === "" && preg_match('/^int/', $field_type)) {
+            $final_value = "null";
+        } else {
+            $final_value = $valueStringfied;
+        }
+
+        return $valueStringfied;
+    }
+
 }
